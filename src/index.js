9f0ea78ffa886e7c14ee4d89858cd5663bbe8d62
deepak-kumar39 <deepakkumarrr39@gmail.com>
2021-03-03 14:56:09 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-var http = require("http");
+// var http = require("http");
 
-const httpServer = http.createServer(handleServer);
+// const httpServer = http.createServer(handleServer);
 
-
-function handleServer(req, res) {
+// httpServer.listen(8081)
+// function handleServer(req, res) {
   
-}
+// }
 
-module.exports = httpServer;
+// module.exports = httpServer;
+const express = require("express");
+const app = express();
+app.get("/welcome",function(request,response){
+    response.status(200).send("Welcome to Dominos!");
+})
+
+app.get("/",function(req,res){
+    res.sendStatus(404);
+})
+app.get("/contact",function(request,response){
+   response.status(200).json({phone: '18602100000',
+   email: 'guestcaredominos@jublfood.com'})
+});
+app.listen(8081,function(){
+console.log("server started on port 8081");
+});
+
